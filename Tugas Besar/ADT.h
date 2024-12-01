19ebd0fa819b2501f2e5d418dcca8796cd9962da
cerealtwist <ventriloquist225@gmail.com>
2024-12-02 01:39:54 +0700

    Penambahan komentar pada ADT.h

--- Tugas Besar/ADT.h
+++ Tugas Besar/ADT.h
@@ -1,36 +1,65 @@
 #ifndef ADT_H_INCLUDED
 #define ADT_H_INCLUDED
+
 #include <iostream>
 #include <algorithm>
 #include <sstream>
 #include <string>
 using namespace std;
+
+// Alias untuk pointer pada node Stack
 typedef struct Stack *address;
 
-struct Stack{
-    string data;
-    address next;
-    address prev;
-};
-struct List{
-    address first;
-    address last;
+// Struktur data untuk Stack node
+struct Stack {
+    string data;      // Data yang disimpan pada node
+    address next;     // Pointer ke node berikutnya
+    address prev;     // Pointer ke node sebelumnya
 };
 
+// Struktur untuk Double Linked List
+struct List {
+    address first;    // Pointer ke node pertama dalam list
+    address last;     // Pointer ke node terakhir dalam list
+};
+
+// Membuat sebuah node baru dengan data 'x'
 address createNode(string x);
+
+// Menginisialisasi list kosong dengan pointer pertama dan terakhir bernilai nullptr
 void createList(List &L);
+
+// Menambahkan node baru di akhir list
 void insertLast(List &L, address p);
+
+// Menambahkan node baru di awal list
 void insertFirst(List &L, address p);
+
+// Menyisipkan node baru setelah node dengan data 'prec'
 void insertAfter(List &L, address p, string prec);
+
+// Menghapus node terakhir dalam list dan menyimpan pointer node yang dihapus
 void deleteLast(List &L, address &p);
+
+// Menghapus node pertama dalam list dan menyimpan pointer node yang dihapus
 void deleteFirst(List &L, address &p);
+
+// Menghapus node setelah node dengan data 'prec' dan menyimpan pointer node yang dihapus
 void deleteAfter(List &L, address &p, string prec);
+
+// Menyalin data dari node yang mengandung substring 'pSub' ke node setelah substring 'qSub'
 void copyPaste(List &L, const std::string &pSub, const std::string &qSub);
-void undoRedo (List &L, List &V);
-void wordCounter (List L);
-void printInfo (List L);
+
+// Fitur undo dan redo yang terkait dengan operasi pada list
+void undoRedo(List &L, List &V);
+
+// Menghitung jumlah kata dalam setiap node dalam list
+void wordCounter(List L);
+
+// Mencetak seluruh informasi data dalam list
+void printInfo(List L);
+
+// Mencari substring 'cari' dalam list dan mengembalikan pointer ke node yang relevan
 void searching(List L, address &p, const std::string &cari);
-
-
 
 #endif // ADT_H_INCLUDED
